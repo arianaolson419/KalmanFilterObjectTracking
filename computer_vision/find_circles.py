9f3672390a05aa3b1746cdcf006055025cd7a933
arianaolson419 <arianaolson419@gmail.com>
2018-11-26 12:52:41 -0500

    Add methods to detect circles in images and video

--- computer_vision/find_circles.py
+++ computer_vision/find_circles.py
@@ -1,16 +1,77 @@
 import numpy as np
-import cv2 as cv
+import cv2
 
 class CVOperations(object):
     def __init__(self):
         pass
 
+    # TODO: delete this function and put image drawing back into detect_circles_image.
+    def draw_circles_image(self, circles, output):
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype('int')
+
+            for (x, y, r) in circles:
+                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        return output
+
+    def draw_circles_frame(self, circles, frame):
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype('int')
+
+            for (x, y, r) in circles:
+                cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        
+    def detect_circles_np_array(self, image):
+        output = image.copy()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+        ouptut = self.draw_circles_image(circles, output)
+        cv2.imshow('output', np.hstack([image, output]))
+        cv2.waitKey(0)
+
+    def detect_circles_image(self, image_name='soccer.jpg'):
+        image = cv2.imread(image_name)
+        self.detect_circles_np_array(image)
+
+    def detect_circles_video(self):
+        """Detect circles in a video using Hough Circles.
+        """
+        cap = cv2.VideoCapture(0)
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+                self.draw_circles_frame(circles, frame)
+
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+        # Release everything if job is finished.
+        cap.release()
+        cv2.destroyAllWindows()
+
+
     def record_and_save(self, output_name='output'):
-        cap = cv.VideoCapture(0)
+        """ Records a video using the webcam and saves it as a .avi file.
+        
+        Parameters
+        ----------
+        output_name: the name of the saved video. Do not include '.avi' in the
+            name. Defaults to 'output'.
+        """
+        cap = cv2.VideoCapture(0)
 
         # Define the codec and create VideoWriter object.
-        fourcc = cv.VideoWriter_fourcc(*'XVID')
-        out = cv.VideoWriter('{}.avi'.format(output_name),
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('{}.avi'.format(output_name),
                 fourcc,
                 20.0,
                 (640,480))
@@ -18,13 +79,13 @@ class CVOperations(object):
         while(cap.isOpened()):
             ret, frame = cap.read()
             if ret == True:
-                frame - cv.flip(frame, 0)
+                frame - cv2.flip(frame, 0)
 
                 # Write the flipped frame.
                 out.write(frame)
 
-                cv.imshow('frame', frame)
-                if cv.waitKey(1) & 0xFF == ord('q'):
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             else:
                 break
@@ -32,8 +93,8 @@ class CVOperations(object):
         # Release everything if job is finished.
         cap.release()
         out.release()
-        cv.destroyAllWindows()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     op = CVOperations()
-    op.record_and_save()
+    op.detect_circles_video()
