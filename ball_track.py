4f17f37ccfe48ab5a29819ae6e585325d100d205
arianaolson419 <arianaolson419@gmail.com>
2018-12-04 14:35:36 -0500

    Add trackbars for each parameter

--- ball_track.py
+++ ball_track.py
@@ -14,7 +14,6 @@ class BallTrack(object):
 
         self.bridge = CvBridge()
         self.cv_op = CVOperations()
-        image_size = (160, 120, 3)
         self.current_image = None
 
     def trackbar(self):
@@ -27,12 +26,12 @@ class BallTrack(object):
             self.cv_op.dp = max(0.001, dp / 10.)
 
         def on_min_dist_trackbar(min_dist):
-            self.cv_op.min_dist = min_dist
+            self.cv_op.min_dist = max(1, min_dist)
 
-        def on_param_one_slider(param_one):
+        def on_param_one_trackbar(param_one):
             self.cv_op.param_one = param_one
 
-        def on_param_two_slider(param_two):
+        def on_param_two_trackbar(param_two):
             self.cv_op.param_two = param_two
 
         def on_min_radius_trackbar(min_radius):
@@ -44,7 +43,11 @@ class BallTrack(object):
         # Note: sliders use integer values, so user input may be altered before
         # the CVOperations object is updated.
         cv2.createTrackbar('dp', title_window , int(self.cv_op.dp * 10), 100, on_dp_trackbar)
-        cv2.createTrackbar('min_dist', title_window, int(self.cv_op.min_dist), 500, on_min_dist_trackbar)
+        cv2.createTrackbar('min_dist', title_window, int(self.cv_op.min_dist), 160, on_min_dist_trackbar)
+        cv2.createTrackbar('param_one', title_window, int(self.cv_op.param_one), 500, on_param_one_trackbar)
+        cv2.createTrackbar('param_two', title_window, int(self.cv_op.param_two), 500, on_param_two_trackbar)
+        cv2.createTrackbar('min_radius', title_window, int(self.cv_op.min_radius), 160, on_min_radius_trackbar)
+        cv2.createTrackbar('max_radius', title_window, int(self.cv_op.max_radius), 160, on_max_radius_trackbar)
 
     def find_circles(self, img):
         img = self.bridge.imgmsg_to_cv2(img, desired_encoding="rgb8")
