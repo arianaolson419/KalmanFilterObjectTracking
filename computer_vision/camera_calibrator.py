4f97f7c92988f3db252ddf6b629ba7013c23b972
robertsiegel <rsiegel812@gmail.com>
2018-12-04 13:52:11 -0500

    Cleaned up, comments added

--- computer_vision/camera_calibrator.py
+++ computer_vision/camera_calibrator.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
-
 import rospy
 from sensor_msgs.msg import CameraInfo
 
 class CameraCalibrator():
+    """
+       This class accesses the camera calibration info (focal length and image center) and uses these
+       values to find the real object location from the image data
+    """
     def __init__(self):
-    	rospy.init_node('camera_calibrator')
-    	self.K = []
+        rospy.init_node('camera_calibrator')
+        self.K = [] # Initializes the camera calibration values
         self.retrieve_camera_info()
 
+    def retrieve_camera_info(self):
+        """ Accesses the camera info from the rostopic """
+        rospy.Subscriber("/sensor_msgs/camera_info", CameraInfo, self.callback)
+
     def callback(self, msg):
-    	print 'hi'
+        """ Callback function to store the calibration data """
         self.K = msg.K
 
-    def retrieve_camera_info(self):
-        rospy.Subscriber("/sensor_msgs/camera_info", CameraInfo, self.callback)
+    def get_object_distance(self, real_height, img_height, img_center):
+    """ Find the distance to the object given the object height """
+        pass
 
 if __name__ == '__main__':
     c = CameraCalibrator()
