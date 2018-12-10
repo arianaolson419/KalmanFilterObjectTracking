7384714b78dfa5a3d7638a820b1905c9653a8360
robertsiegel <rsiegel812@gmail.com>
2018-12-10 15:27:41 -0500

    Distance measurement using ground interface technique

--- computer_vision/camera_calibrator.py
+++ computer_vision/camera_calibrator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import CameraInfo
+import time 
 
 class CameraCalibrator():
     """
@@ -9,21 +10,29 @@ class CameraCalibrator():
     """
     def __init__(self):
         rospy.init_node('camera_calibrator')
-        self.K = [] # Initializes the camera calibration values
-        self.retrieve_camera_info()
+        self.K = [] # Initialize camera parameters    
+        # Subscriber accesses the camera info from the rostopic
+        camera_sub = rospy.Subscriber('/camera/camera_info', CameraInfo, self.callback) 
+        rospy.rostime.wallsleep(0.1) # waits to subscriber has time to access data
+        
+        # Obtain relevant values from K
+        self.fx = self.K[0]
+        self.fy = self.K[4]
+        self.cx = self.K[2]
+        self.cy = self.K[5]
 
-    def retrieve_camera_info(self):
-        """ Accesses the camera info from the rostopic """
-        rospy.Subscriber("/sensor_msgs/camera_info", CameraInfo, self.callback)
-
+        self.camera_height = .15 # height of camera off ground in meters
     def callback(self, msg):
-        """ Callback function to store the calibration data """
+    	""" Callback function to store the calibration data """
         self.K = msg.K
 
-    def get_object_distance(self, real_height, img_height, img_center):
-    """ Find the distance to the object given the object height """
-        pass
+    def get_object_distance(self, radius_measured, y_img):
+        """ Find the distance to the object given the y value at which the object touches the ground """
+        y_ground = y_img - radius_measured
+        z = (-self.camera_height * self.fy)/(y_ground - self.cy)
+        return z
 
 if __name__ == '__main__':
     c = CameraCalibrator()
-    print c.K
+    print c.fy
+
